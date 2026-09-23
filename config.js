4510fb63114a6681f66894559c2e774e04b6c193
Vuk Stanojev <77501834+VukStanojev@users.noreply.github.com>
2026-09-23 12:08:17 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// ------------------------------------------------------------------
+//  THE ONLY FILE YOU NEED TO EDIT.
+//  Paste the Google Apps Script web-app URL between the quotes
+//  (it ends in /exec). Leave it empty to test locally: results are
+//  then offered as a download instead of being sent.
+// ------------------------------------------------------------------
+const CONFIG = {
+  SUBMIT_URL: 'https://script.google.com/macros/s/AKfycbzNyduVhC8hXPCpRfg56_aD5VyAzpLKpLs5rLL8GEX8EHuZKishLALp-9pBiBOapdOq/exec',
+  TEST_VERSION: 'telfor2026-v1',
+  CONTACT_EMAIL: 'vukstns@gmail.com',
+};
